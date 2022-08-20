b9f5bf899b131eaa220364254bf79a577ae0377b
abhi180196 <110031162+abhi180196@users.noreply.github.com>
2022-08-20 12:44:14 +0530

    Update dataLayer.js
    update data layer

--- js/dataLayer.js
+++ js/dataLayer.js
@@ -1,36 +1,54 @@
-var digitalData = {
+//DATA LAYER
+// window.adobeDataLayer = window.adobeDataLayer || [];
 
-     page: {
-   
-        pageInfo: {
-   
-              
-            pageName: document.title,
-              
-   
-            referringURL: document.referrer,
-   
-            
-        }
-   
-      
-   
-        },
-   
-        userInfo : {
-   
-           loginStatus : "false",
-   
-           username : "",
-   
-           bannerClick : "false"
-   
-    }
-   
-   }
-   
-   
-   var username = localStorage.getItem("username");
+var pagename = document.title;
+var url = document.URL;
+var host = window.location.host;
+var d1 = new Date(); 
+var currDate = d1.toISOString().slice(0, 10);
+var currTime = d1.toLocaleTimeString();
+var currDevice = "";
+if(screen.width <= 421)
+	currDevice = "Mobile";
+else if (screen.width > 421 && screen.width <= 1023)
+	currDevice = "Tablet";
+else
+	currDevice = "Desktop";
+
+setInterval(() => {
+	d1 = new Date()
+	currTime = d1.toLocaleTimeString()
+}, 1000);
+window.digitalData = {
+		page: {
+			pageName: pagename,
+			referringURL: document.referrer,
+			pageURL : url,
+			deviceType: currDevice,
+			page_level: 3,
+		},
+		category: {
+			primaryCategory: "",
+			subCategory1: "",
+			subCategory2: "",
+			subCategory3: "",
+
+		},
+		attributes: {
+			country: "IN",
+			date: currDate,
+			time: currTime,
+			domain: host
+		},
+		user:{
+			loginStatus: "false",
+			username : "",
+			bannerClick : "false"
+		},
+		product : [],
+}
+
+var username = localStorage.getItem("username");
    
    var loggedInStatus = localStorage.getItem("loginStatus");
    
@@ -41,9 +59,10 @@ var digitalData = {
        digitalData.userInfo.username = username;
    
    }
-   
-   if(localStorage.getItem("bannerClick")){
+
+if(localStorage.getItem("bannerClick")){
    
        digitalData.userInfo.bannerClick = localStorage.getItem("bannerClick")
    
    }
+
